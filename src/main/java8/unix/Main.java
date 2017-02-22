9fc0752d587924bac6ef69c7a866a1f4c8574969
Satoshi <tokusato0630@gmail.com>
2017-02-23 00:42:20 +0900

    added unix4j maven configuration

---
+++ src/main/java8/unix/Main.java
@@ -0,0 +1,56 @@
+package unix;
+
+import static util.CommonUtil.*;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+
+public class Main {
+
+	Logger logger = Logger.getLogger("UNIX_TEST");
+
+
+	public static void main(String[] args) {
+
+		String rootDir = FILE_UNIX_PATH1;
+		//ファイルを読み込み
+		new Main().readFileAndCapitalize(rootDir);
+
+	}
+
+	/**
+	 * 特定のファイルファイルを読み込み、その内容を大文字にする。
+	 * */
+	private void readFileAndCapitalize(String rootDir) {
+
+		Path p = Paths.get(rootDir);
+		StringBuilder sb = new StringBuilder();
+		String result;
+		try (DirectoryStream<Path> ds = Files.newDirectoryStream(p)){
+			Iterator<Path> ite = ds.iterator();
+			while (ite.hasNext()) {
+				Path path = (Path) ite.next();
+				System.out.println(path.toRealPath().toString());
+				sb.append( path.toRealPath().toString());
+
+				//Objectsクラスのnull判定メソッドを利用して
+				if (!Objects.isNull(sb)) logger.info(sb.toString());
+				//UnixUtilクラスのメソッドを実行
+				result = UnixUtil.catAndCapitalize(path.toFile());
+				System.out.println(result);
+			}
+		}catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+	}
+
+
+
+}
