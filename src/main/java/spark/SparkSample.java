bde24bb4d0bf1da7a4c31e1c5a6c945acec548fa
tokus <tokus@DESKTOP-J92QUTI>
2017-03-22 12:19:28 +0900

    added action when accessed to /helloWorld

--- src/main/java/spark/SparkSample.java
+++ src/main/java/spark/SparkSample.java
@@ -5,6 +5,19 @@ import static spark.Spark.*;
 public class SparkSample {
 
 	public static void main(String[] args) {
+
+		//to change the port. it is deprecated but easy.
+//		setPort(6789);
+
+        //beforeによるリクエスト前の処理
+        before((req, res) -> {
+            System.out.println("全てのリクエスト共通");
+        });
+
+        before("/helloWorld", (req, res) -> {
+            System.out.println("peculiar action. only performed when accessed to /helloWorld");
+        });
+
 		// run and try to access -> http://localhost:4567/helloWorld
 		get("/helloWorld", (req, res) -> "Wonderfull WebServer is included");
 
