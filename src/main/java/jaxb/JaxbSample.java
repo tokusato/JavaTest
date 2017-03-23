c2521c0609464567d388b7dc2f76b8131e71752d
Satoshi <tokusato0630@gmail.com>
2017-03-23 22:29:21 +0900

    added oracle connection bean

--- src/main/java/jaxb/JaxbSample.java
+++ src/main/java/jaxb/JaxbSample.java
@@ -17,8 +17,6 @@ public class JaxbSample {
 	/**
 	 * testing the function of jaxb<br>
 	 * <p>
-	 * aaa
-	 *
 	 * @author n
 	 * @param execution
 	 *            arguments
@@ -38,15 +36,18 @@ public class JaxbSample {
 		// generate Java Object
 //		TestBean tb1 = new TestBean(1, "Tom");
 		TestBean tb1 = new TestBean(1, null);
+		OracleConnector oc = new OracleConnector("localhost", 1522, "pdborcl", "appuser", "apppass1234");
 
 		// Marshalするものは、beanでないといけない。
 		// 標準出力へXML書き出し( for 動作確認 )
 		JAXB.marshal(tb1, System.out);
 
 		String xmlPath = CommonUtil.ROOT_PATH + "jaxb/bean.xml";
+		String oraXmlPath = CommonUtil.ROOT_PATH + "jaxb/oraBean.xml";
 
 		try {
 			JAXB.marshal(tb1, new FileOutputStream(new File(xmlPath)));
+			JAXB.marshal(oc, new FileOutputStream(new File(oraXmlPath)));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -125,5 +126,94 @@ class TestBean {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+}
+
+/**
+ * Oracle DB との接続を表現するクラス
+ * */
+class OracleConnector{
+
+	/** server name */
+	private String serverName;
+	/** port number */
+	private int portNumber;
+	/** sid */
+	private String sid;
+	/** user */
+	private String user;
+	/** password */
+	private String password;
+
+	/** constructor using all fields */
+	public OracleConnector(String serverName, int portNumber, String sid, String user, String password) {
+		super();
+		this.serverName = serverName;
+		this.portNumber = portNumber;
+		this.sid = sid;
+		this.user = user;
+		this.password = password;
+	}
+	/**
+	 * @return serverName
+	 */
+	public String getServerName() {
+		return serverName;
+	}
+	/**
+	 * @param serverName セットする serverName
+	 */
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+	/**
+	 * @return portNumber
+	 */
+	public int getPortNumber() {
+		return portNumber;
+	}
+	/**
+	 * @param portNumber セットする portNumber
+	 */
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+	/**
+	 * @return sid
+	 */
+	public String getSid() {
+		return sid;
+	}
+	/**
+	 * @param sid セットする sid
+	 */
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	/**
+	 * @return user
+	 */
+	public String getUser() {
+		return user;
+	}
+	/**
+	 * @param user セットする user
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password セットする password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 
 }
